f4c44dda55bed0ca580ee35fdeb7a73b4d2b0855
Collodel Umberto <collodelu@centralbankmalta.org>
2024-06-12 12:09:56 +0200

    forecasted future gdp values with arima model - from last weo

--- 2.1_estimate_pandemic scenario.R
+++ 2.1_estimate_pandemic scenario.R
@@ -1,6 +1,37 @@
 ############# Script to generate pandemic shocks and their impact on Co2 emissions
 ################################################################################
 
+# Load the necessary libraries
+library(forecast)
 
-####### Read GDP data: -----
+####### Load WEO GDP data: -----
 
+growth <- NGDP_R %>% 
+  filter(ifscode == 1) %>% 
+  select(-ifscode) %>% 
+  slice(-nrow(.))
+
+
+# Convert the data into a time series object and fit ARIMA:
+
+fit <- ts(growth$NGDPR, start=c(1950), frequency=1) %>% 
+  auto.arima()
+
+# Forecast future values:
+
+future_years <- 2029:2100
+
+forecasted_values <- forecast(fit, h=length(future_years)) %>% 
+  as_tibble() %>% 
+  select(`Point Forecast`) %>%
+  mutate(Year = 2029:2100) %>% 
+  setNames(c("NGDPR","Year"))
+
+# Bind with historical data: 
+  
+gdp_forecast <- rbind(growth,forecasted_values) %>% 
+  mutate(SCENARIO = "Historical") 
+    
+
+
+    
